b54c039f3daee8979267ea2a24b99c27627ecb23
elisas17 <70175096+elisas17@users.noreply.github.com>
2022-08-12 17:37:49 -0300

    Add files via upload

---
+++ produtos.php
@@ -0,0 +1,7 @@
+<?php
+$produto = [
+    ['id' => 1, 'nome' => 'Executive', 'valor' => 30.00, 'tipo' => 'Pesada', 'peso' => '1,000g', 'descricao' => 'Arroz, feijão preto, frango grelhado e saladas', 'imagem' => '  images/01.jpg'],
+    ['id' => 2, 'nome' => 'Ligth', 'valor' => 32,00, 'tipo' => 'Leve', 'peso' => '500g', 'descricao' => 'Salada, abóbora ralada, ovos mexidos com peito de peru, tomate e beringela', 'imagem' => 'images/02.jpg'],
+    ['id' => 3, 'nome' => 'Truck Driver', 'valor' => 20.00, 'tipo' => 'Pesado', 'peso' => '1000g', 'descricao' => 'Suco, Molho crocante de iscas de frango', 'imagem' => 'images/03.jpg'],
+    ['id' => 4, 'nome' => 'Dinner', 'valor' => 17.00, 'tipo' => 'Padrão', 'peso' => '700g', 'descricao' => 'Diversas saladas', 'imagem' => 'images/04.jpg']
+];
