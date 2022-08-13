f99ebde089d801872c1b613d99fd6030f8c1d0ec
elisas17 <70175096+elisas17@users.noreply.github.com>
2022-08-12 22:10:20 -0300

    Add files via upload

---
+++ mapaFacul/function/real.php
@@ -0,0 +1,6 @@
+<?php 
+    function BRL($val){
+        return 'R$ '. number_format($val,2,",",".");
+  }
+  ?>
+  
---
+++ mapaFacul/pages/home.php
@@ -0,0 +1,25 @@
+<?php 
+ include 'dados/produtos.php'; ?>
+ <?php
+ include 'function/real.php'; ?>
+
+
+  <h1 class="h1s">MARMITAS</h1>
+   <div class="produtos">
+  <?php
+    foreach($produto as $pdt): ?> 
+    
+   <div class="marmitas">       
+        <a id = "product" href = "index.php"?p=produto.php&id />
+        <?php = $pdt ['id']; ?>    
+        <img src="$ pdt['imagem']" style="width:200px; border-radius: 10%;">    
+        <p></p>
+          <?=$pdt['nome']?>
+        <p></p>
+        <p id="valor"><?=BRL($pdt['valor'])?></p></a>    
+    </div>  
+    </div>
+</div>
+  <?php 
+  endforeach; ?> 
+ 
